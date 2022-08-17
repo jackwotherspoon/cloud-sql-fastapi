1ddf15f2f4a72533023ef645835f215cf3a02494
jackwotherspoon <jackwoth@google.com>
2022-08-17 02:27:49 +0000

    fix: fix /votes get endpoint

--- app/schemas.py
+++ app/schemas.py
@@ -2,16 +2,9 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class VoteBase(BaseModel):
+class Vote(BaseModel):
     candidate: str
-
-
-class Vote(VoteBase):
     time_cast: datetime
     
     class Config:
         orm_mode = True
-
-
-class VoteCreate(VoteBase):
-    pass
