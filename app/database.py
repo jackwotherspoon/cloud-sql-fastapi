eca96773d5d9200b629aed0ea44bae39d090a16a
jackwotherspoon <jackwoth@google.com>
2022-08-20 16:40:45 +0000

    feat: add IAM database authentication with Python Connecotr

--- app/database.py
+++ app/database.py
@@ -12,13 +12,15 @@ load_dotenv()
 def getconn():
     # if env var PRIVATE_IP is set to True, use private IP Cloud SQL connections
     ip_type = IPTypes.PRIVATE if os.getenv("PRIVATE_IP") == True else IPTypes.PUBLIC
+    # if env var DB_IAM_USER is set, use IAM database authentication
+    user, enable_iam_auth = (os.getenv("DB_IAM_USER"), True) if os.getenv("DB_IAM_USER") else (os.getenv("DB_USER"), False)
     # initialize Cloud SQL Python connector object
-    with Connector(ip_type=ip_type) as connector:
+    with Connector(ip_type=ip_type, enable_iam_auth=enable_iam_auth) as connector:
         conn = connector.connect(
             os.getenv('INSTANCE_CONNECTION_NAME'),
             "pg8000",
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
+            user=user,
+            password=os.getenv("DB_PASS", ""),
             db=os.getenv("DB_NAME")
         )
         return conn
